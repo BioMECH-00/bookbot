cf5bd0146e30cc9f59756faa9ad4b6a119c22bb2
BioMECH-00 <jpwelsh@gmail.com>
2024-06-03 15:01:53 +0900

    added get_book_text(), get_book_word_count(), get_book_character_count()

---
+++ main.py
@@ -0,0 +1,34 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    word_count = get_book_word_count(book_path)
+    letter_count = get_book_character_count(book_path)
+
+
+   
+
+def get_book_text(path):
+     with open(path) as f:
+        return f.read()
+
+def get_book_word_count(path):
+    text = get_book_text(path)
+    words = text.split()
+    return len(words)
+
+def get_book_character_count(path):
+    text = get_book_text(path)
+    lowered_text = text.lower()
+    words = lowered_text.split()
+    letter_dict = {}
+    for word in words:
+        for letter in word:
+            if letter in letter_dict:
+                letter_dict[letter] += 1
+            else:
+                letter_dict[letter] = 1
+    return letter_dict
+
+
+main()
+    
