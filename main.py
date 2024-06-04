551541ed352c8b895ebc4863ee56a70b4db3116d
BioMECH-00 <jpwelsh@gmail.com>
2024-06-04 14:24:18 +0900

    completed project

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     word_count = get_book_word_count(text)
-    print(word_count)
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{word_count} were found in the document")
     letter_count = get_book_character_count(text)
-    print(letter_count)
+    sorted = sort_letter_dict(letter_count)
+    for letters in sorted:
+        print(f"The letter '{letters["char"]}' was found {letters["num"]} times.")
+    print("--- END OF REPORT ---")
 
 
    
@@ -28,6 +32,19 @@ def get_book_character_count(text):
             else:
                 letter_dict[letter] = 1
     return letter_dict
+
+def sort_on(dict):
+    return dict["num"]
+
+def sort_letter_dict(letter_dict):
+    sorted_letter_dict = []
+    for entries in letter_dict:
+        if entries.isalpha():
+            sorted_letter_dict.append({"char": entries, "num":letter_dict[entries]})
+    sorted_letter_dict.sort(reverse=True, key=sort_on)
+    return sorted_letter_dict
+        
+
 
 
 main()
